c4869d691fe7b65243751f64f8ce1eaf15b54fda
Frederik <git@caf-tranberg.dk>
2023-12-05 13:05:02 +0100

    update jsonreader

--- PowerHouse/JSON read/readJSON.c
+++ PowerHouse/JSON read/readJSON.c
@@ -2,51 +2,95 @@
 #include "cJSON.c"
 #include "cJSON.h"
 
-
-int main(void) { 
+int readBatt(int minutes) { 
     // open the file 
     FILE *fp = fopen("testfile-multiline.json", "r"); 
     if (fp == NULL) { 
         printf("Error: Unable to open the file.\n"); 
         return 1; 
     } 
-  
-    // read the file contents into a string 
-    char buffer[1024]; 
-    int len = fread(buffer, 1, sizeof(buffer), fp); 
-    fclose(fp); 
-  
-    // parse the JSON data 
-    cJSON *json = cJSON_Parse(buffer); 
-    if (json == NULL) { 
-        const char *error_ptr = cJSON_GetErrorPtr(); 
-        if (error_ptr != NULL) { 
-            printf("Error: %s\n", error_ptr); 
+    char line[1024]; // Assuming lines are no longer than 100 characters
+
+    // Read and print each line of the file
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        cJSON *json = cJSON_Parse(line); 
+        if (json == NULL) { 
+            const char *error_ptr = cJSON_GetErrorPtr(); 
+            if (error_ptr != NULL) { 
+                printf("Error: %s\n", error_ptr); 
+            } 
+            cJSON_Delete(json); 
+            return 1; 
         } 
+
+        char *json_str = cJSON_Print(json); 
+        //printf("%s\n", json_str);  
+    
+        // access the JSON data 
+        cJSON *message = cJSON_GetObjectItemCaseSensitive(json, "message"); 
+    
+    
+        char *json_str2 = cJSON_Print(message);
+        //printf("%s\n", json_str2);
+
+        cJSON *current = cJSON_GetObjectItemCaseSensitive(message, "current"); 
+        if (cJSON_IsNumber(current)) { 
+            printf("Batt-I/O: %f\n", current->valuedouble); 
+        } 
+    
+        // delete the JSON object 
         cJSON_Delete(json); 
+        }
+
+        fclose(fp); // Don't forget to close the file when done
+    return 0;
+}
+
+int readSolar(int minutes) { 
+    // open the file 
+    FILE *fp = fopen("testfile-multiline.json", "r"); 
+    if (fp == NULL) { 
+        printf("Error: Unable to open the file.\n"); 
         return 1; 
     } 
+    char line[1024]; // Assuming lines are no longer than 100 characters
 
-    char *json_str = cJSON_Print(json); 
-    printf("%s\n", json_str);  
-  
-    // access the JSON data 
-    cJSON *message = cJSON_GetObjectItemCaseSensitive(json, "message"); 
- 
- 
-    char *json_str2 = cJSON_Print(message);
-    printf("%s\n", json_str2);
+    // Read and print each line of the file
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        cJSON *json = cJSON_Parse(line); 
+        if (json == NULL) { 
+            const char *error_ptr = cJSON_GetErrorPtr(); 
+            if (error_ptr != NULL) { 
+                printf("Error: %s\n", error_ptr); 
+            } 
+            cJSON_Delete(json); 
+            return 1; 
+        } 
 
-    cJSON *power_out = cJSON_GetObjectItemCaseSensitive(message, "pv_power"); 
-    if (cJSON_IsNumber(power_out)) { 
-        printf("Power_In: %i\n", power_out->valueint); 
-    } 
-    cJSON *current = cJSON_GetObjectItemCaseSensitive(message, "current"); 
-    if (cJSON_IsNumber(current)) { 
-        printf("Amps_In: %f\n", power_out->valuedouble); 
-    } 
-  
-    // delete the JSON object 
-    cJSON_Delete(json); 
-    return 0; 
+        char *json_str = cJSON_Print(json); 
+        //printf("%s\n", json_str);  
+    
+        // access the JSON data 
+        cJSON *message = cJSON_GetObjectItemCaseSensitive(json, "message"); 
+    
+    
+        char *json_str2 = cJSON_Print(message);
+        //printf("%s\n", json_str2);
+
+        cJSON *power_out = cJSON_GetObjectItemCaseSensitive(message, "pv_power"); 
+        if (cJSON_IsNumber(power_out)) { 
+            printf("Power_In: %i\n", power_out->valueint); 
+        } 
+    
+        // delete the JSON object 
+        cJSON_Delete(json); 
+        }
+
+        fclose(fp); // Don't forget to close the file when done
+    return 0;
+}
+
+
+int main(void) { 
+    readBatt(8);
 }
